dfee3a1f4f0c2122707d6157cd3847820c8f206e
Nathan Stark <nathan.e.stark@gmail.com>
2019-07-02 16:54:01 -0400

    Exporting things

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-export default function Hi() { console.log("hi"); }
+import FileSource from './file-source/file-source';
+import CachedFileSource, { ICachedFileSource } from './file-source/cached-file-source';
+import MemFileSource from './file-source/mem-file-source';
+import RandomAccessFileSource from './file-source/random-access-file-source';
+import ReadWriteLockFileSource from './file-source/read-write-lock-file-source';
+import TransactionFileSource, { ITransaction } from './file-source/transaction-file-source';
+
+export { 
+    FileSource,
+    CachedFileSource,
+    ICachedFileSource,
+    MemFileSource,
+    RandomAccessFileSource,
+    ReadWriteLockFileSource,
+    TransactionFileSource,
+    ITransaction
+};
