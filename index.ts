c7fae026b75fbfe177b71c59621b2ccf9dc0e221
Nathan Stark <nathan.e.stark@gmail.com>
2019-07-02 16:57:33 -0400

    Magic?

---
+++ index.ts
@@ -0,0 +1,17 @@
+import FileSource from './src/file-source/file-source';
+import CachedFileSource, { ICachedFileSource } from './src/file-source/cached-file-source';
+import MemFileSource from './src/file-source/mem-file-source';
+import RandomAccessFileSource from './src/file-source/random-access-file-source';
+import ReadWriteLockFileSource from './src/file-source/read-write-lock-file-source';
+import TransactionFileSource, { ITransaction } from './src/file-source/transaction-file-source';
+
+export { 
+    FileSource,
+    CachedFileSource,
+    ICachedFileSource,
+    MemFileSource,
+    RandomAccessFileSource,
+    ReadWriteLockFileSource,
+    TransactionFileSource,
+    ITransaction
+};
